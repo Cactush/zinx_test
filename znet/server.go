4d0c75fcd668e0e1b6d47640ca667612ef2a65d6
gw <gu123owei>
2019-12-12 15:35:14 +0800

    zinx 基础架构搭建

---
+++ znet/server.go
@@ -0,0 +1,76 @@
+package znet
+
+import (
+	"errors"
+	"fmt"
+	"github.com/Cactush/zinx_test/ziface"
+	"net"
+	"time"
+)
+
+type Server struct {
+	Name      string
+	IPVersion string
+	IP        string
+	Port      int
+}
+
+func (s *Server) Start() {
+	fmt.Printf("[START] Server listenner at IP: %s, Port %d, is starting\n", s.IP, s.Port)
+
+	go func() {
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
+		if err != nil {
+			fmt.Println("resolve tcp addr err:", err)
+			return
+		}
+
+		listenner, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("listen", s.IPVersion, "err", err)
+			return
+		}
+		fmt.Println("start Zinx server  ", s.Name, " succ, now listenning...")
+		var cid uint32
+		cid = 0
+		for {
+			conn, err := listenner.AcceptTCP()
+			if err != nil {
+				fmt.Println("Accept err", err)
+				continue
+			}
+			dealConn := NewConnection(conn, cid, CallBackToClient)
+			cid++
+			go dealConn.Start()
+		}
+	}()
+}
+func (s *Server) Stop() {
+	fmt.Println("[STOP] Zinx server , name ", s.Name)
+}
+
+func (s *Server) Serve() {
+	s.Start()
+	for {
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func NewServer(name string) ziface.IServer {
+	s := &Server{
+		Name:      name,
+		IPVersion: "tcp4",
+		IP:        "0.0.0.0",
+		Port:      7777,
+	}
+	return s
+}
+
+func CallBackToClient(conn *net.TCPConn, data []byte, cnt int) error {
+	fmt.Println("[Conn Handle] CallBackToClient ...")
+	if _, err := conn.Write(data[:cnt]); err != nil {
+		fmt.Println("write back buf err ", err)
+		return errors.New("CallBackToClient error")
+	}
+	return nil
+}
